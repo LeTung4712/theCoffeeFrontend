c829b0268dc19318950fec4a89309ab2b59fb307
Le Thanh Tung <iamrobotdiy@gmail.com>
2024-03-12 17:18:26 +0700

    tạo ui mainpage

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app id="inspire" >
+    <v-content style="background-color: white">
+      <headerIntro></headerIntro>
+      <v-container rounded-0 class="fill-height" fluid>
+        <router-view />
+
+      </v-container>
+    </v-content>
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+
+import { mapState, mapMutations } from "vuex";
+import headerIntro from "./header/headerIntro";
+import Footer from "./footer/Footer";
+
+export default {
+  name: "Layout",
+
+  components: {
+    headerIntro,
+    Footer
+  },
+
+  props: {
+    source: String
+  },
+  data: () => ({
+    expandOnHover: false
+  }),
+  computed: {
+    ...mapState(["Customizer_drawer"])
+  },
+
+  methods: {
+    ...mapMutations({
+      setCustomizerDrawer: "SET_CUSTOMIZER_DRAWER"
+    })
+  }
+};
+</script>
+
+<style>
+.fill-height {
+  align-items: start !important;
+  margin-top: 42px;
+
+}
+</style>
